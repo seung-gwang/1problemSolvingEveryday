6c71515e433e7905cb8e942caaa5dbc2eb95b581
seunggwang <sgroh1997@gmail.com>
2023-05-21 00:36:05 +0900

    2023-05-20 PS

---
+++ 2023_5/20230520.cpp
@@ -0,0 +1,15 @@
+/*
+https://school.programmers.co.kr/learn/courses/30/lessons/181951
+*/
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int a;
+    int b;
+    cin >> a >> b;
+    cout << "a = " << a << "\n";
+    cout << "b = " << b << "\n";
+    return 0;
+}
