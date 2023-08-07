8708d4ea0b6ae77b4e562508417cb1b0a635b840
seunggwang <sgroh1997@gmail.com>
2023-08-07 14:56:56 +0900

    2023-08-03 PS

---
+++ 2023_8/20230803.cpp
@@ -0,0 +1,12 @@
+/*
+https://school.programmers.co.kr/learn/courses/30/lessons/181948
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void) {
+    const string st = "!@#$%^&*(\\'\"<>?:;";
+    cout << st << endl;
+    return 0;
+}
