cf018e5e90eaf2010c7e333c834e3b0bc1dc3e5a
seunggwang <sgroh1997@gmail.com>
2023-08-07 14:57:23 +0900

    2023-08-05 PS

---
+++ 2023_8/20230805.cpp
@@ -0,0 +1,16 @@
+/*
+https://school.programmers.co.kr/learn/courses/30/lessons/181936
+*/
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int number, int n, int m) {
+    int answer = 0;
+    if(number % n == 0 && number % m == 0) {
+        answer = 1;
+    }
+    
+    return answer;
+}
