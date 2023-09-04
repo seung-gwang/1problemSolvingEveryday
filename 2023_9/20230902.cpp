be8e49a743a2ae9e7b2870ff6be1478c5584ad98
seunggwang <sgroh1997@gmail.com>
2023-09-04 20:55:52 +0900

    2023-09-02 PS

---
+++ 2023_9/20230902.cpp
@@ -0,0 +1,12 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/181952
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string str;
+    cin >> str;
+    cout << str;
+    return 0;
+}
